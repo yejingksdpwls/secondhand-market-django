cf08e688f305818f56d67cdc7787bdbfbccae099
yejingksdpwls <yejingksdpwls@gmail.com>
2024-12-23 21:45:23 +0900

    ADD : 상품 목록, 등록, 삭제, 수정, 찜 기능 추가

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,10 @@ from products import views
 
 app_name = "products"
 urlpatterns = [
+    path("",views.products, name="products"),
+    path("<int:pk>/product_detail/", views.product_detail, name="product_detail"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>/update/", views.update, name="update"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
+    path("<int:pk>/like/",views.like, name="like"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.decorators.http import require_http_methods
+from products.forms import ProductForm
 
 # Create your views here.
 def main(request):
     return render(request, "products/main.html")
+
+
+def products(request):
+    products = Product.objects.all().order_by("-created_at")
+    context = {"products":products}
+    return render(request, "products/products.html", context)
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {"product":product}
+    return render(request, "products/product_detail.html", context)
+
+
+@require_http_methods(["GET","POST"])
+def create(request):
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                product = form.save(commit=False)
+                product.author = request.user
+                product.save()
+                return redirect("products:product_detail", product.pk)
+        else:
+            form = ProductForm()
+            context = {"form":form}
+            return render(request, "products/create.html", context)
+    else:
+        return redirect("accounts:login")
+    
+
+@login_required
+@require_http_methods(["GET","POST"])
+def update(request, pk):
+    product = Product.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            product = form.save()
+            return redirect("products:product_detail", product.pk)
+    else:
+        form = ProductForm(instance=product)
+    context = {"product":product}
+    return render(request, "products/update.html", context)
+
+
+@login_required
+@require_http_methods(["POST"])
+def delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.user == product.author:
+        product.delete()
+        return redirect("products:products")
+    return redirect("product_detail", pk)
+
+
+@require_http_methods(["POST"])
+def like(request, pk):
+    if request.user.is_authenticated:
+        product = get_object_or_404(Product, pk=pk)
+        if request.user != product.author:
+            if request.user in product.like_user.all():
+                product.like_user.remove(request.user)
+            else:
+                product.like_user.add(request.user)
+        else:
+            messages.error(request, "본인의 물품은 찜할 수 없습니다.")
+        return redirect("products:product_detail", pk)
+    else:
+        return redirect("accounts:login")
