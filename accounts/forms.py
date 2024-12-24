1e45acc1dd54f3e57c1ed76619cf126df2234756
yejingksdpwls <yejingksdpwls@gmail.com>
2024-12-24 20:43:56 +0900

    ADD : 프로필 사진 업로드, 판매/찜 목록 기능 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django import forms
 
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = UserCreationForm.Meta.fields + ("email", "first_name", "last_name")
+        fields = UserCreationForm.Meta.fields + ("email", "first_name", "last_name", "image",)
 
 class CustomUserChangeForm(UserChangeForm):
 
@@ -18,6 +19,19 @@ class CustomUserChangeForm(UserChangeForm):
             "email",
             "first_name",
             "last_name",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 비밀번호 필드가 없다면 help_text를 설정할 필요 없음
+        if "password" in self.fields:
+            del self.fields["password"]  # 비밀번호 필드를 아예 폼에서 제거
+
+class CustomUserProfileChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "image",
         )
 
     def __init__(self, *args, **kwargs):
--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("password/", views.password, name="password"),
     path("<int:pk>/follow/", views.follow, name="follow"),
     path("<int:pk>/profile/", views.profile, name="profile"),
+    path("<int:pk>/profile_update/", views.profile_update, name="profile_update"),
 ]
